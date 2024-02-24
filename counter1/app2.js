05ea0f3f0e9159c39434cb29b6a12dd3e446181f
bart2n <a.bartunak@gmail.com>
2024-02-24 22:15:18 +0300

    secondcommit

--- counter1/app2.js
+++ counter1/app2.js
@@ -1,3 +1,27 @@
 let counter = 0;
 
-const 
+const btns = document.querySelectorAll('.btn');
+const color = document.querySelector("#value");
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('increase')){
+           counter++;
+        }
+        else if(styles.contains('decrease')){
+           counter--;
+        }
+        else{
+            counter = 0;
+        }
+        if(counter>0){
+            value.style.color = "green";
+        }
+        if(counter<0){
+            value.style.color = "red";
+        }
+
+        value.textContent = counter;
+    });
+});
