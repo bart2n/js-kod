7dff1498ca642e429aea303346854420e48d7bbb
bart2n <a.bartunak@gmail.com>
2024-03-01 21:27:11 +0300

    8thcommit

--- menu3/app.js
+++ menu3/app.js
@@ -72,70 +72,69 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
-const sectionCenter = document.querySelector('.section-center');
-const container = document.querySelector('.btn-container');
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
 
 
-//load items
-window.addEventListener('DOMContentLoaded', function() {
-   displayMenuItems(menu);
-   const categories = menu.reduce(function(values,item){
+window.addEventListener("DOMContentLoaded",function(){
+  displayMenuItems(menu);
+
+  const categories = menu.reduce(function(values,item){
     if(!values.includes(item.category)){
-      values.push(item.category)
+      values.push(item.category);
     }
-    return values
-   },['all'])
-    const categoryBtns = categories.map(function(category){
-      return `<button class="filter-btn" data-id=${category}>${category}</button>`
-    }).join("");
-    container.innerHTML = categoryBtns ;
+    return values;
+  },['all']);
+  const categoryBtns = categories.map(function(category){
+    return `<button class="filter-btn" data-id="${category}">${category}</button>`;
+  }).join("");
+  container.innerHTML = categoryBtns;
 
-//filterbtns
-const filterBtns = document.querySelectorAll('.filter-btn');
-filterBtns.forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
+
+
+
+
+  //filterbtns
+  const filterBtns = document. querySelectorAll(".filter-btn");
+  filterBtns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
     const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function(menuItem) {
-       if(menuItem.category === category) {
-        return menuItem;
-       }
-    });
-    if (category === "all") {
-        displayMenuItems(menu)
-    } else {
-        displayMenuItems(menuCategory)
+    const menuCategory = menu.filter(function(menuItem){
+      if(menuItem.category === category){
+      return menuItem;
       }
     });
-  });
+    if(category === "all"){
+      displayMenuItems(menu)
+    } else {
+      displayMenuItems(menuCategory)
+
+    }
+    });
+});
+});
 
 
-   });
 
 
 
 
 
 
-
-
-
-//displaymenuitems
 function displayMenuItems(menuItems){
-  let displayMenu = menuItems.map(function(item) {
-    return `
-      <article class="menu-item">
-        <img src="${item.img}" class="photo" alt="${item.title}">
-        <div class="item-info">
-          <header>
-            <h4>${item.title}</h4>
-            <h4 class="price">$${item.price}</h4>
-          </header>
-          <p class="item-text">${item.desc}</p>
-        </div>
-      </article>
-    `;
+  let displayMenu = menuItems.map(function(item){
+    return `<article class="menu-item">
+      <img src="${item.img}" class="photo" alt="${item.title}">
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">$${item.price}</h4>
+        </header>
+        <p class="item-text">${item.desc}</p>
+      </div>
+    </article>
+  `;
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-
-} 
+}
